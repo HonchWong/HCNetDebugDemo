08db4fad627853b8e0f7cddba3279d594d1f328d
HonchWong <honchwong404@gmail.com>
2019-02-11 12:11:30 +0800

    初始化工程

---
+++ HCNetDebugDemo/HCNetDebugDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCNetDebugDemo
+//
+//  Created by 黄鸿昌 on 2019/2/11.
+//  Copyright © 2019 黄鸿昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
